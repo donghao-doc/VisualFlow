d18223f1ca5d3f2774aa124a2a530710d4ab3f41
BarryDong <286084845@qq.com>
2023-11-28 15:25:25 +0800

    feat: 页面基础布局

---
+++ src/components/AppNav.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app-navigator">AppNav</div>
+</template>
+
+<style scoped>
+.app-navigator {
+  border: 2px solid red;
+  height: 48px;
+  box-shadow: rgb(0 0 0 / 8%) 0 1px 0;
+  position: relative;
+  z-index: 5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppNav from '@/components/AppNav.vue'
+</script>
 
 <template>
-  <div>Home 页面</div>
+  <div class="app-wrapper">
+    <header>
+      <app-nav />
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
+
+<style scoped>
+.app-wrapper {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+}
+
+main {
+  height: calc(100% - 48px);
+}
+</style>
